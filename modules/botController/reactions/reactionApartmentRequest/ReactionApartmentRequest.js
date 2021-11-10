45f592918e7769bd13ff8c3889768035419feb24
xetemilekex@snakebite.com <xetemilekex@snakebite.com>
2021-11-10 10:24:55 -0800

    self check rate limit, tamper control

--- modules/botController/reactions/reactionApartmentRequest/ReactionApartmentRequest.js
+++ modules/botController/reactions/reactionApartmentRequest/ReactionApartmentRequest.js
@@ -19,6 +19,9 @@ class ReactionApartmentRequest {
 	 */
 	constructor(app, params) {
 		this.app = app;
+		this.allowance = 100000; // 100 second time allotment 
+		this.date = new Date();
+		this.time = date.getTime();
 		this.db = level('cinnabarapts', async function (err, db) {
 			if (err) throw err;
 
@@ -60,6 +63,7 @@ class ReactionApartmentRequest {
 		if (!ev.msg.match(/\b(lease|rent|available|free)( an)? +apartments?\b/)
 			&& !ev.msg.match(/\bapartments? *(available\b|to rent\b|to lease\b|free)\b/)
 		) {
+			this._checkAllowance(7000);
 			this.module.actionAddress.enqueue(
 				char.id,
 				ev.char.id,
@@ -67,11 +71,13 @@ class ReactionApartmentRequest {
 				true,
 				100
 			);
+			this.allowance -= 7000; // Addresses cost 7 seconds
 			return;
 		}
 
 		// Check if we already have an apartment
 		if (await this._alreadyHasApartment(ev.char.id)) {
+			this._checkAllowance(7000);
 			this.module.actionAddress.enqueue(
 				char.id,
 				ev.char.id,
@@ -79,7 +85,8 @@ class ReactionApartmentRequest {
 				false,
 				100
 			);
-			return
+			this.allowance -= 7000;
+			return;
 		}
 
 		// We could just call the API directly with the steps. But by letting
@@ -103,29 +110,27 @@ class ReactionApartmentRequest {
 		}
 		let { unitNr, target } = outcome;
 
-		// Here we call the different commands directly against the API. We
-		// sleep a little inbetween commands to avoid triggering the flood
-		// filter, and to make it look more "natural".
+		this._checkAllowance(100000);
 		await char.call('address', {
 			msg: "Sure thing, let me get that ready for you.",
 			charId: target.id,
 		});
-		await sleep(5000);
+		await sleep(1000);
 		await char.call('useExit', { exitKey: 'out' });
-		await sleep(5000);
+		await sleep(1000);
 		await char.call('useExit', { exitKey: 'up' });
-		await sleep(5000);
+		await sleep(1000);
 		let area = await char.call('createArea', {
 			name: `Apartment ${unitNr}`,
 			ParentID: char.inRoom.area.id
 		});
-		await sleep(5000);
+		await sleep(1000);
 		await char.call('setLocation', {
 			locationId: area.id,
 			type: 'area',
 			private: true
 		});
-		await sleep(5000);
+		await sleep(1000);
 		let createExitResult = await char.call('createExit', {
 			keys:  [ unitNr, target.name + " " + target.surname ],
 			name: `Apartment ${unitNr}`,
@@ -134,15 +139,15 @@ class ReactionApartmentRequest {
 			travelMsg: `goes inside apartment ${unitNr}`,
 			hidden: true
 		});
-		await sleep(5000);
+		await sleep(1000);
 		await char.call('useExit', { exitKey: unitNr });
-		await sleep(5000);
+		await sleep(1000);
 		await char.call('setRoom', {
 			name: `Apartment ${unitNr}`,
 			desc: "The apartment is empty.",
 			areaId: area.id
 		});
-		await sleep(5000);
+		await sleep(1000);
 		await char.call('setExit', {
 			exitKey: 'back',
 			name: 'To Hallway',
@@ -151,21 +156,21 @@ class ReactionApartmentRequest {
 			arriveMsg: `arrives from apartment ${unitNr}.`,
 			travelMsg: "leaves the apartment."
 		});
-		await sleep(5000);
+		await sleep(3000);
 		await char.call('requestSetRoomOwner', {
 			roomId: createExitResult.targetRoom.id,
 			charId: target.id
 		});
-		await sleep(5000);
+		await sleep(1000);
 		await char.call('requestSetAreaOwner', {
 			areaId: area.id,
 			charId: target.id
 		});
-		await sleep(5000);
+		await sleep(1000);
 		await char.call('teleportHome');
-		await sleep(5000);
+		await sleep(1000);
 		await char.call('address', {
-			msg: replaceTags("says ,\"Alright, you’re all set up with your new apartment. Here are your keys, you’re in unit {unitNr} Thank you for choosing Cinnabar Prism Apartments, we hope you enjoy your stay. Feel free to have a look around the facilities.\"\n((You can get there with the commands: `go out`, `go up`, `go apartment {unitNr}` (or alternatively `go {charName} {charSurname}` or simply `go {unitNr}`) ))\n((Make sure to accept the room and area requests in the Realm panel to the far left.))", {
+			msg: replaceTags("says ,\"Alright, you’re all set up with your new apartment. Here are your keys, you’re in unit {unitNr} Thank you for choosing Cinnabar Prism Apartments, we hope you enjoy your stay. Feel free to have a look around the facilities.\"\n((You can get there with the commands: `go out`, `go up`, `go apartment {unitNr}` (or alternatively `go {charName} {charSurname}` or simply `go {unitNr}`) ))\n((Make sure to accept the room and area requests in the Realm panel to the far left.))\n\n((I will now go in sleep mode, it may take some time for me to respond to more requests. Zzz.))", {
 				unitNr,
 				charName: target.name,
 				charSurname: target.surname
@@ -173,6 +178,12 @@ class ReactionApartmentRequest {
 			pose: true,
 			charId: target.id
 		});
+		this.allowance = 0; // This is the know result of the above.
+		if(await this.db.get(target.id) === 'WAITING') {
+			// The db failed to update
+			console.log("Error updating db with new unit, deleting row.");
+			await this.db.del(target.id);
+		}
 
 	}
 
@@ -183,6 +194,7 @@ class ReactionApartmentRequest {
 			return false;
 		} catch (err) {
 			console.log(`No apartment found for character ${charId}: ${err}`);
+			await this.db.put(charId, 'WAITING');
 			return false;
 		}
 	}
@@ -207,6 +219,21 @@ class ReactionApartmentRequest {
         }
         return current;
     }
+
+	_updateAllowance() {
+		let old = this.time;
+		this.time = date.getTime();
+		let difference = Math.floor((this.time - old) / 1000) * 1000; // round to the most recent second
+		this.allowance = Math.min(this.allowance + difference, 100000); // allowance cannot exceed 100 seconds
+	}
+
+	_checkAllowance(cost) {
+		this._updateAllowance();
+		if(this.allowance < cost) {
+			await sleep(Math.min(this.allowance - cost, 100000)); // will never have to sleep more than 100 seconds to recover
+			this._updateAllowance(); 
+		}
+	}
 
 	dispose() {
 		this.module.charEvents.unsubscribe(this._onCharEvent);
